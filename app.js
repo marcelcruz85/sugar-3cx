2a4b01130aab2c6490d6a9d3877da4defc48e7e0
dotgital <support@dotgital.com>
2020-10-11 13:37:01 -0500

    CDR Class Added

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ server.on('connection', function(socket) {
   
     socket.on('data', function(chunk) {
         Cdr.cdr = chunk.toString().split(',');
-        console.log('Array: ' + chunk.toString().split(',') );
+        //console.log('Array: ' + chunk.toString().split(',') );
         console.log('Json: ' + JSON.stringify(Cdr.toJson()) );
     });
 
--- cdr.js
+++ cdr.js
@@ -6,6 +6,29 @@ class CDR {
             timeStart: null,
             timeAnswered: null,
             timeEnd: null,
+            fromNo: null,
+            toNo: null,
+            fromType: null,
+            toType: null,
+            finalType: null,
+            reasonTerminated: null,
+            historyId: null,
+            callId: null,
+            dialNo: null,
+            finalNumber: null,
+            chain: null,
+            missedQueueCalls: null,
+            fromDn: null,
+            toDn: null,
+            reasonChanged: null,
+            finalDn: null,
+            billRate: null,
+            billCost: null,
+            billName: null,
+            fromDispname: null,
+            toDispname: null,
+            finalDispname: null,
+            billCode: null,
         }
     }
     toJson() {
@@ -13,6 +36,29 @@ class CDR {
         this.cdrJson.timeStart = this.cdr['1'] ? new Date( this.cdr['1'] ).toISOString() : null;
         this.cdrJson.timeAnswered = this.cdr['2'] ? new Date( this.cdr['2'] ).toISOString() : null;
         this.cdrJson.timeEnd = this.cdr['3'] ? new Date( this.cdr['3'] ).toISOString() : null;
+        this.cdrJson.fromNo = this.cdr['4'];
+        this.cdrJson.toNo = this.cdr['5'];
+        this.cdrJson.fromType = this.cdr['6'];
+        this.cdrJson.toType = this.cdr['7'];
+        this.cdrJson.finalType = this.cdr['8'];
+        this.cdrJson.reasonTerminated = this.cdr['9'];
+        this.cdrJson.historyId = this.cdr['10'];
+        this.cdrJson.callId = this.cdr['11'];
+        this.cdrJson.dialNo = this.cdr['12'];
+        this.cdrJson.finalNumber = this.cdr['13'];
+        this.cdrJson.chain = this.cdr['14'];
+        this.cdrJson.missedQueueCalls = this.cdr['15'];
+        this.cdrJson.fromDn = this.cdr['16'];
+        this.cdrJson.toDn = this.cdr['17'];
+        this.cdrJson.reasonChanged = this.cdr['18'];
+        this.cdrJson.finalDn = this.cdr['19'];
+        this.cdrJson.billRate = this.cdr['20'];
+        this.cdrJson.billCost = this.cdr['21'];
+        this.cdrJson.billName = this.cdr['22'];
+        this.cdrJson.fromDispname = this.cdr['23'];
+        this.cdrJson.toDispname = this.cdr['24'];
+        this.cdrJson.finalDispname = this.cdr['25'];
+        this.cdrJson.billCode = this.cdr['26'];
 
         return this.cdrJson;
     }
