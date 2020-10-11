a43e2043cc057a69c9c37340dd238609fab64f13
dotgital <support@dotgital.com>
2020-10-11 12:10:10 -0500

    CDR Class Added

--- cdr.js
+++ cdr.js
@@ -4,15 +4,15 @@ class CDR {
         this.cdrJson = {
             duration: null,
             timeStart: null,
-            timeanswered: null,
+            timeAnswered: null,
             timeEnd: null,
         }
     }
     toJson() {
         this.cdrJson.duration = this.cdr['0'].replace('Call ', '');
         this.cdrJson.timeStart = this.cdr['1'] ? new Date( this.cdr['1'] ).toISOString() : null;
-        this.cdrJson.timeanswered = this.cdr['2'];
-        this.cdrJson.timeEnd = this.cdr['3'];
+        this.cdrJson.timeAnswered = this.cdr['2'] ? new Date( this.cdr['2'] ).toISOString() : null;
+        this.cdrJson.timeEnd = this.cdr['3'] ? new Date( this.cdr['3'] ).toISOString() : null;
 
         return this.cdrJson;
     }
