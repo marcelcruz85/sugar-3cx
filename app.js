5c5b4384f923cb5e06f610ac66a4ade572c095e2
dotgital <support@dotgital.com>
2020-10-11 11:49:47 -0500

    CDR Class Added

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ server.on('connection', function(socket) {
     console.log('A new connection has been established.');
   
     socket.on('data', function(chunk) {
-        Cdr.cdr = chunk.toString().split(',');
+        Cdr.cdr = chunk;
         console.log('Data received from client: ' + Cdr.toJson() );
     });
 
--- cdr.js
+++ cdr.js
@@ -1,9 +1,14 @@
 class CDR {
     constructor(cdr){
-        this.cdr = cdr;
+        this.cdr = cdr.toString().split(',');
+        this.cdrJson = {
+            duration: null
+        }
     }
     toJson() {
-        return this.cdr;
+        this.cdrJson.duration = this.cdr['0'];
+
+        return this.cdrJson;
     }
 }
 
